fc59dd5ff80850de5f654b993e476f42269f51e1
isamu <isamumuraguchi.mail@gmail.com>
2026-07-02 22:12:53 +0900

    スマホでの自動ズームを防止

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,15 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
   title: "物々交換",
   description: "誰とでもいつでも物々交換できるサイト",
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
 };
 
 export default function RootLayout({
