7adcd6b28b94e446c805967b1649b42f57e74b74
isamu <isamumuraguchi.mail@gmail.com>
2026-07-04 01:39:22 +0900

    kaeccoロゴを160pxに変更

--- components/Header.tsx
+++ components/Header.tsx
@@ -162,7 +162,7 @@ export default function Header({
             </svg>
           </button>
           {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img src="/kaecco-logo.png" alt="kaecco" style={{ height: "240px", width: "auto" }} />
+          <img src="/kaecco-logo.png" alt="kaecco" className="h-40 w-auto" />
         </div>
 
         <div className="flex items-center gap-1">
