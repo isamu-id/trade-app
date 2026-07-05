338a95171cd706579f96a48f26b8c9a052e77dea
isamu <isamumuraguchi.mail@gmail.com>
2026-07-05 18:41:35 +0900

    Linkの重複importを修正

--- app/offers/[id]/page.tsx
+++ app/offers/[id]/page.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import Link from "next/link";
 import { createClient } from "@/lib/supabase/server";
 import { notFound, redirect } from "next/navigation";
 import OfferActions from "./offer-actions";
